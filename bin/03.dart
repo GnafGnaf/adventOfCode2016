afa04b9b0aa3b7536d650b836a0e4088541663cf
gnafgnaf <jvwershof@gmail.com>
2016-12-28 21:16:32 +0100

    03: Part II: This is ugly AF but I realy don't enjoy this exercise, so what ever... it's my spare time after all.

--- bin/03.dart
+++ bin/03.dart
@@ -7,14 +7,35 @@ main() {
   var triangleValidator = new TriangleValidator();
   var validTriangleCount = 0;
 
-  for (var line in file.readAsLinesSync()) {
-    var sides = line.trim().split(new RegExp(' +'));
-
+  for (var triangle in _parseLines(file.readAsLinesSync())) {
     if (triangleValidator.isSideLengthValid(
-        int.parse(sides[0]), int.parse(sides[1]), int.parse(sides[2]))) {
+        triangle[0], triangle[1], triangle[2])) {
       validTriangleCount++;
     }
   }
 
   print(validTriangleCount);
+}
+
+List<List<int>> _parseLines(List<String> lines) {
+  var triangles = [];
+  for (var iterator = lines.iterator; iterator.moveNext();) {
+    var line1 = _parseLine(iterator.current);
+    if (iterator.moveNext()) {
+      var line2 = _parseLine(iterator.current);
+      if (iterator.moveNext()) {
+        var line3 = _parseLine(iterator.current);
+        triangles.add([line1[0], line2[0], line3[0]]);
+        triangles.add([line1[1], line2[1], line3[1]]);
+        triangles.add([line1[2], line2[2], line3[2]]);
+      }
+    }
+  };
+
+  return triangles;
+}
+
+List<String> _parseLine(String line) {
+  return line.trim().split(new RegExp(' +')).map((string) => int.parse(string))
+      .toList();
 }
